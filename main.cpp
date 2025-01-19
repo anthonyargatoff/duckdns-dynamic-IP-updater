4c9bbfb03cdf81ba5251faf2e266b5416e678569
anthonyargatoff <anthonyargatoff@gmail.com>
2025-01-19 15:15:58 -0800

    Initial cpp

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include <string.h>
+#include <chrono>
+#include <thread>
+
+struct DuckDNSParams
+{
+  std::string domainName;
+  std::string APIToken;
+};
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
+std::string getRequest(std::string&& url)
+{
+  CURL *handle;
+  CURLcode res;
+  std::string readBuffer;
+
+  handle = curl_easy_init();
+  if (handle == NULL)
+  {
+    std::cerr << "Failed to initialize cULR" << std::endl;
+    return "";
+  }
+
+  curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteCallback);
+  curl_easy_setopt(handle, CURLOPT_WRITEDATA, &readBuffer);
+  res = curl_easy_perform(handle);
+  curl_easy_cleanup(handle);
+
+  if (res != 0)
+  {
+    std::cerr << "cURL error: " << curl_easy_strerror(res) << std::endl;
+  }
+  std::cout << "Get request response: " << readBuffer << std::endl;
+  return readBuffer;
+}
+
+// TODO: Get env var
+const std::string duckDNSRequest(const DuckDNSParams& params, std::string& newIp)
+{
+  return getRequest("https://www.duckdns.org/update?domains=" + params.domainName + "&token=" + params.APIToken + "&ip=" + newIp);
+}
+
+// TODO: Get env var
+const std::string ipRequest()
+{
+  return getRequest("https://api.ipify.org/");
+}
+
+int main()
+{
+  if (std::getenv("API_KEY") == nullptr || std::getenv("DOMAIN_NAME") == nullptr) {
+    std::cerr << "Must include API_KEY and DOMAIN_NAME" << std::endl;
+    return 1;
+  }
+
+  const DuckDNSParams getParams = {std::getenv("DOMAIN_NAME"), std::getenv("API_KEY")};
+
+  // Initial duckdns request
+  std::string oldResult = ipRequest();
+  if (oldResult.empty())
+  {
+    std::cout << "Initial request to api.ipify failed." << std::endl;
+  }
+  duckDNSRequest(getParams, oldResult);
+
+  // Loop to keep check and updating the dynamic dns
+  while (1)
+  {
+    std::string newResult = ipRequest();
+    if (newResult.compare(oldResult)) // IP has changed. Make duckdns request
+    {
+      duckDNSRequest(getParams, newResult);
+      oldResult = newResult;
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(60)); // Sleep for 60 seconds
+  }
+
+  return 0;
+}
